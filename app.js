d1a0e8e240ab2fb9d4bdb4e70e3d53bc6b2af55c
Ry <68780806+Ry-Projects@users.noreply.github.com>
2021-05-16 05:05:08 -0400

    Updating fonts semantic html, & sidebar layout

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const splashText = document.querySelector("#changingText")
-const words = ["DEVELOPER", "STUDENT"]
-let i = 0
-let j = 0
-let currPhrase = []
-let isDeleting = false
-let isEnd = false
+const splashText = document.querySelector("#changing-text");
+const words = ["DEVELOPER", "STUDENT", "CREATOR"];
+let i = 0;
+let j = 0;
+let currWord = [];
+let isDeleting = false;
+let isEnd = false;
 
 function writeSplash() {
-  isEnd = false
+  isEnd = false;
   if (i < words.length) {
     if (!isDeleting && j <= words[i].length) {
-      currPhrase.push(words[i][j])
-      splashText.innerHTML = currPhrase.join("");
-      j++
+      currWord.push(words[i][j]);
+      splashText.innerHTML = currWord.join("");
+      j++;
     }
     if (isDeleting && j <= words[i].length) {
-      currPhrase.pop(words[i][j])
-      splashText.innerHTML = currPhrase.join("");
-      j--
+      currWord.pop(words[i][j]);
+      splashText.innerHTML = currWord.join("");
+      j--;
     }
     if (j == words[i].length) {
-      isDeleting = true
-      isEnd = true
+      isDeleting = true;
+      isEnd = true;
     }
     if (isDeleting && j == 0) {
-      currPhrase = []
-      isDeleting = false
-      i++
+      currWord = [];
+      isDeleting = false;
+      i++;
       if (i == words.length) {
-        i = 0
+        i = 0;
       }
     }
   }
-  const time = (isEnd) ? 3000 : (isDeleting) ? 100 : 115
-  setTimeout(writeSplash, time)
+  const time = isEnd ? 3000 : isDeleting ? 100 : 115;
+  setTimeout(writeSplash, time);
 }
 
-writeSplash()
+writeSplash();
