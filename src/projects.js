2ffd99d31913adcd330efb8f52d6ef935f336955
Ry <68780806+ryhull@users.noreply.github.com>
2021-10-01 16:31:40 -0400

    Nav logo, favicon, slight font changes, new project

--- src/projects.js
+++ src/projects.js
@@ -1,30 +1,30 @@
 const PROJECT_LIST = [
     {
+        name: "Price : Rent",
+        description:
+            "Single page app which allows users to input any supported Canadian city, and then quickly recieve a baseline decision on whether it would be better to purchase property in the city, or instead rent. It does this through scraping listings from a real estate site to return up to date estimates using the price-to-rent ratio. I designed and built the front-end of the app at Hack the North 2021.",
+        tags: ["app", "recommended"],
+        image: "src/images/sample.jpg",
+        demoLink: "https://price-to-rent.herokuapp.com/static/index.html",
+        codeLink: "https://github.com/zhouj892/htn2021",
+    },
+
+    {
         name: "Hangman For The Film Fan",
         description:
             "A hangman style game where a random movie quote is given, and the hidden word is its corresponding movie. The code tracks and saves the current and best winning streaks to localStorage, and additionally protects against repeating the same movie. Made using HTML, CSS, and vanilla JavaScript.",
         tags: ["game", "recommended"],
-        image: "images/sample.jpg",
+        image: "src/images/sample.jpg",
         demoLink: "https://ryhull.github.io/hangman-for-the-film-fan/",
         codeLink: "https://github.com/ryhull/hangman-for-the-film-fan",
     },
 
     {
-        name: "Buy or Rent",
-        description:
-            "Aliquam at lorem elementum mi fringilla euismod. Donec sed nisl dapibus, tempor arcu eu, interdum ante. Fusce sit amet diam sapien. Ut sit amet sapien sed tortor tristique rutrum. Cras at molestie diam. Quisque eu justo in metus vulputate fermentum.",
-        tags: ["recommended", "app"],
-        image: "images/sample.jpg",
-        demoLink: "https://ryhull.github.io/buy-or-rent/",
-        codeLink: "https://github.com/ryhull/buy-or-rent",
-    },
-
-    {
         name: "Portfolio Site",
         description:
-            "Minimalist dark-themed portfolio designed by myself. Features a typewriter effect in the hero section, and a section for displaying and filtering portfolio projects by category.",
+            "Minimalist dark-themed portfolio designed by myself. Features a typewriter effect, and a section for displaying and filtering projects by category.",
         tags: ["recommended", "landing"],
-        image: "images/sample.jpg",
+        image: "src/images/sample.jpg",
         demoLink: "https://ryhull.github.io",
         codeLink: "https://github.com/ryhull/ryhull.github.io",
     },
@@ -32,9 +32,9 @@ const PROJECT_LIST = [
     {
         name: "Fylo Landing Page",
         description:
-            "Aliquam at lorem elementum mi fringilla euismod. Donec sed nisl dapibus, tempor arcu eu, interdum ante. Fusce sit amet diam sapien. Ut sit amet sapien sed tortor tristique rutrum. Cras at molestie diam. Quisque eu justo in metus vulputate fermentum.",
+            "Professionally designed landing page I built using assets provided by Frontend Mentor. After recreating their design, I added more features to page, such as a mobile navigation menu.",
         tags: ["landing"],
-        image: "images/sample.jpg",
+        image: "src/images/sample.jpg",
         demoLink: "https://ryhull.github.io/fylo/",
         codeLink: "https://github.com/ryhull/fylo",
     },
@@ -44,7 +44,7 @@ const PROJECT_LIST = [
         description:
             "Aliquam at lorem elementum mi fringilla euismod. Donec sed nisl dapibus, tempor arcu eu, interdum ante. Fusce sit amet diam sapien. Ut sit amet sapien sed tortor tristique rutrum. Cras at molestie diam. Quisque eu justo in metus vulputate fermentum.",
         tags: ["recommended", "app"],
-        image: "images/sample.jpg",
+        image: "src/images/sample.jpg",
         demoLink: "https://ryhull.github.io/taskr/",
         codeLink: "https://github.com/ryhull/taskr",
     },
