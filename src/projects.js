b88fba5a69aeca485dbd370a2118ac6cc4c2b2d6
Ry <68780806+ryhull@users.noreply.github.com>
2022-01-24 21:33:55 -0500

    Added project images

--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,7 @@ const PROJECT_LIST = [
         description:
             "Full stack application allowing registered users to easily add, update, sort, and track their gift cards on the fly instead of having to manually write and update values on the physical cards themselves. All passwords are hashed and salted, and session management is used to control access. I designed and built this using JavaScript, PHP, and MySQL to help with my terrible gift card hoarding problem.",
         tags: ["app", "recommended"],
-        image: "src/images/sample.jpg",
+        image: "src/images/mygiftcards.png",
         demoLink: "https://csunix.mohawkcollege.ca/~sa000847264/mygiftcards",
         codeLink: "#",
     },
@@ -13,7 +13,7 @@ const PROJECT_LIST = [
         description:
             "Prototype for a full stack application that would serve as a hub for beginner designers/developers to recruit each other and collaborate on projects. The current version allows users to search and filter through existing posts, or add one of their own. I developed this using JavaScript, PHP, and MySQL. This is a long-term project I plan on adding onto as I pick up further skills and experience!",
         tags: ["app", "recommended"],
-        image: "src/images/sample.jpg",
+        image: "src/images/rookruit.png",
         demoLink: "https://ryhull.github.io/rookruit/",
         codeLink: "https://github.com/ryhull/rookruit",
     },
@@ -23,7 +23,7 @@ const PROJECT_LIST = [
         description:
             "Single page application which allows users to input any supported Canadian city, and then quickly receive a baseline decision on whether it would be better to purchase property in the city, or instead rent. It does this through scraping listings from a real estate site to return up to date estimates using the price-to-rent ratio. I designed and built the front end of the app in a team of three at Hack the North 2021.",
         tags: ["app", "recommended"],
-        image: "src/images/sample.jpg",
+        image: "src/images/pricerent.png",
         demoLink: "https://price-to-rent.herokuapp.com",
         codeLink: "https://github.com/zhouj892/htn2021",
     },
@@ -33,7 +33,7 @@ const PROJECT_LIST = [
         description:
             "Game where a random movie quote is given, and the player must guess all letters of its corresponding movie without running out of lives. The code tracks and saves the current and best winning streaks using localStorage.",
         tags: ["game"],
-        image: "src/images/sample.jpg",
+        image: "src/images/hangman.png",
         demoLink: "https://ryhull.github.io/hangman-for-the-film-fan/",
         codeLink: "https://github.com/ryhull/hangman-for-the-film-fan",
     },
@@ -43,7 +43,7 @@ const PROJECT_LIST = [
         description:
             "Minimalist dark-themed personal portfolio page designed by myself. Features a fully responsive design, a typewriter effect, and a section for dynamically displaying and filtering projects by category.",
         tags: ["landing"],
-        image: "src/images/sample.jpg",
+        image: "src/images/portfolio.png",
         demoLink: "https://ryhull.github.io",
         codeLink: "https://github.com/ryhull/ryhull.github.io",
     },
@@ -53,7 +53,7 @@ const PROJECT_LIST = [
         description:
             "A sample landing page I built using a design provided by Frontend Mentor. After completing their design, I added additional functionality to the page, such as a mobile navigation menu.",
         tags: ["landing"],
-        image: "src/images/sample.jpg",
+        image: "src/images/fylo.png",
         demoLink: "https://ryhull.github.io/fylo/",
         codeLink: "https://github.com/ryhull/fylo",
     },
